ace887b7d29bc369b6b4bcb8d6550523bc58588d
Michael Schneider <micha.schneider@sap.com>
2024-07-24 11:31:05 +0200

    Implement GC to draw a rectangle
    Colors are not considered, yet

--- bundles/org.eclipse.swt/Eclipse SWT/skia/org/eclipse/swt/uno/UnoGraphics.java
+++ bundles/org.eclipse.swt/Eclipse SWT/skia/org/eclipse/swt/uno/UnoGraphics.java
@@ -2,69 +2,9 @@ package org.eclipse.swt.uno;
 
 import java.util.*;
 
-import com.sun.star.awt.*;
-import com.sun.star.uno.*;
-
-public class UnoGraphics {
-
-	static long idCounter = 0;
-	static HashMap<Long, UnoGraphics> graphicsMap = new HashMap<>();
-
-	public static UnoGraphics Get(long id) {
-		return graphicsMap.get(Long.valueOf(id));
-	}
-
-
-	private XGraphics xGraphics;
-	private XDevice xDevice;
-	private UnoDrawable unoDrawable;
-	private long id;
-
-	public UnoGraphics(UnoDevice unoDevice, UnoDrawable unoDrawable) {
-		XWindow xWindow = unoDrawable.getXWindow();
-
-		xDevice  = UnoRuntime.queryInterface(XDevice.class, xWindow);
-		xGraphics = xDevice.createGraphics();
-		this.unoDrawable = unoDrawable;
-		registerGraphics();
-	}
-
-	private void registerGraphics() {
-		this.id = idCounter++;
-		graphicsMap.put(Long.valueOf(id), this);
-	}
-
-	public void fillRectangle(int x, int y, int width, int height) {
-		XWindow window = unoDrawable.getXWindow();
-		Rectangle posSize = window.getPosSize();
-
-		xGraphics.setFillColor(255);
-		xGraphics.setLineColor(255);
-		xGraphics.drawRect(x,y, width, height);
-
-		XBitmap bitmap = xDevice.createBitmap(0, 0, posSize.Width, posSize.Height);
-		XDisplayBitmap displayBitmap = xDevice.createDisplayBitmap(bitmap);
-
-		unoDrawable.setXBitmap(bitmap);
-
-//		window.addPaintListener(new PaintListener(xGraphics, displayBitmap));
-	}
-
-	public long getId() {
-		return id;
-	}
-
-
-
-}
-package org.eclipse.swt.uno;
-
-import java.util.*;
-
 import org.eclipse.swt.graphics.*;
 
 import com.sun.star.awt.*;
-import com.sun.star.awt.Rectangle;
 import com.sun.star.uno.*;
 
 public class UnoGraphics {
